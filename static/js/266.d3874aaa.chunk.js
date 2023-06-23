b372f30830e2bf6129dad6b9a225fd6c822568e7
Iryna-Bigdash <114106094+Iryna-Bigdash@users.noreply.github.com>
2023-06-23 21:26:47 +0000

    Deploying to gh-pages from @ Iryna-Bigdash/goit-react-hw-08-phonebook@4d018cfc0d24f4d5050a5a8a9b3da87b827d55e1 🚀

---
+++ static/js/266.d3874aaa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphoneBook=self.webpackChunkphoneBook||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function l(){return(0,i.jsx)("div",{style:{textAlign:"center"},children:(0,i.jsxs)("h1",{style:c.title,children:["Contacts manager welcome page"," "]})})}}}]);
+//# sourceMappingURL=266.d3874aaa.chunk.js.map
